8a63ebfeee46c71e351dfaa21e1d6409a39528c9
Alperen <154828704+AlperenGokten@users.noreply.github.com>
2024-01-26 20:56:32 +0300

    Add files via upload

---
+++ nummenu.py
@@ -0,0 +1,31 @@
+import listeler.listele.py
+import rehber.rehber_ekle.py
+
+
+def nummenu():   
+
+    
+    print("╔═══════════════════╗")
+    print("║ 1-Rehbere ekle    ║")
+    print("║ 2-kayıt düzelt    ║")
+    print("║ 3-kayıtlılar liste║")
+    print("║ 4-kayıt ekle      ║")
+    print("║ 5-kayıt sil       ║")
+    print("║ 6-çıkış           ║")
+    print("╚═══════════════════╝")
+    secim = input()
+
+    if secım == "1" :
+       rehber.rehber_ekle
+       nummenu()
+    if secım == "" :
+
+        nummenu()
+    if secım == "3" :
+    listeler.listele.py 
+        nummenu ()
+    if secım == "" :
+
+        nummenu()
+ except = xx
+nummenu ()
